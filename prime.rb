ce15884d2083978e5459bd8bc1dc1d67904b24b9
Nicholas Levenson <nicklevenson@gmail.com>
2020-10-22 07:58:16 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+
+def prime?(x)
+    i = 2
+    if x > 1
+        while i < x do
+            if x % i == 0
+                return false
+            end
+            i+=1
+        end
+        return true
+    else
+        return false
+    end
+
+end
